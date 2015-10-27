87ac0b38f7bfb79b0d5d43d33eef0e4b519daefc
Dan Silver <dannysilver3@gmail.com>
2015-10-26 22:08:20 -0500

    Removed dependency com.github.ksoichiro:android-observablescrollview :sparkles:

--- app/src/main/java/com/silver/dan/deals/ProductListFragment.java
+++ app/src/main/java/com/silver/dan/deals/ProductListFragment.java
@@ -14,7 +14,6 @@ import android.view.ViewGroup;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
-import com.github.ksoichiro.android.observablescrollview.ScrollState;
 import com.pnikosis.materialishprogress.ProgressWheel;
 
 import java.util.ArrayList;
